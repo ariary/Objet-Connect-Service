abe41b1976ef858e7188ad114bb95bca82316a7f
Antoine Rabenandrasana <antoine.rabenandrasana@etu.unice.fr>
2018-01-16 16:21:25 +0100

    Mise à jour broker

--- server/index.js
+++ server/index.js
@@ -1,24 +1,67 @@
 var request = require("request");
+const mqtt = require('mqtt')
+const client = mqtt.connect('mqtt://broker.hivemq.com')
 
-var treshold=600;
-request("https://maps.googleapis.com/maps/api/distancematrix/json?origins=Cannes&destinations=Nice&mode=driving&departure_time=now&traffic_model=best_guess&language=fr-FR&key=AIzaSyBfwmxGEnUSlilAXOkAkXbZeskaYJ5Gsak", function(error, response, body) {
+var presence = false;
+
+client.on('connect', () => {
+  client.subscribe('mirror/presence');
+})
+
+
+/*client.on('message', (topic, message) => {
+  if(topic === 'mirror/presence') {
+    console.log(message.toString());
+  }
+})*/
+
+client.on('message', (topic, message) => {
+  switch (topic) {
+    case 'mirror/presence':
+      return handlePresence(message)
+    case 'mirror/led':
+      return console.log("mirror led :%s",message)
+  }
+  console.log('No handler for topic %s', topic)
+})
+
+function handlePresence (message) {
+  console.log('new message in topic mirror/presence: %s', message)
+  getTraficservice();
+  getWeatherService();
+}
+
+function getWeatherService(){
+  console.log("send meteo");
+}
+
+
+function  getTraficservice(){
+  var treshold=600;
+  request("https://maps.googleapis.com/maps/api/distancematrix/json?origins=Cannes&destinations=Nice&mode=driving&departure_time=now&traffic_model=best_guess&language=fr-FR&key=AIzaSyBfwmxGEnUSlilAXOkAkXbZeskaYJ5Gsak", function(error, response, body) {
   //pessimistic, optimistic, best_guess;
   try {
   	var obj = JSON.parse(body); //Parsing JSON
 
-  	/*Retrieve duration*/
+  	//Retrieve duration
   	var elements = obj.rows[0].elements;
   	var usual_time = elements[0].duration.value;
   	var current_time = elements[0].duration_in_traffic.value;
-  	/*define traffic condition*/
+  	//define traffic condition
+    // var color="";
   	if (current_time-usual_time>=treshold) {
   		console.log("Bad traffic condition");
+      // color="red";
   	}else if(current_time-usual_time>=-treshold){
   		console.log("Usual traffic condition ");
+      // color="red";
   	} else {
   		console.log("Excellent traffic condition ");
+      // color="red";
   	};
+    client.publish('mirror/led', "red")
   } catch (e){
   	console.error("Parsing error:",e);
   }
-}); 
+  }); 
+}
