82dc1425e4237fc32592b1c43a3410a298825064
meriemchebaane <meriemchebaane93@gmail.com>
2018-01-23 16:16:17 +0100

    delete broker

--- broker/test.js
+++
@@ -1,8 +0,0 @@
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://test.mosquitto.org')
-
-client.on('connect', function () {
-  client.subscribe('presence')
-  client.publish('presence', 'Hello mqtt')
-})
-
