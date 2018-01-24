04892bab065dea4b83434aa8a516300413703b05
Antoine Rabenandrasana <antoine.rabenandrasana@etu.unice.fr>
2018-01-24 12:19:39 +0100

    Ajout e la configuration du miroir (itinéraire favoris)

--- server/index.js
+++ server/index.js
@@ -1,3 +1,28 @@
+//CONFIGURATION MIRROR
+var express = require('express');
+var bodyParser = require('body-parser');
+var app     = express();
+
+var dep = "";
+var dst = "";
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.post('/itineraryPrefs', function(req, res) {
+  //res.send('You sent the name "' + req.body.name + '".');
+  res.send('Changes saved');
+  console.log("New itinary: dep="+req.body.departure+" dest="+req.body.destination );
+  dep = req.body.departure;
+  dst = req.body.destination
+});
+
+app.listen(8080, function() {
+  console.log('Configuration Server running at http://127.0.0.1:8080/');
+});
+
+
+//MQTT
+
 var request = require("request");
 const mqtt = require('mqtt')
 const client = mqtt.connect('mqtt://broker.hivemq.com')
@@ -44,34 +69,39 @@ function getWeatherService(){
 
 
 function  getTraficservice(){
-  var treshold=600;
-  request("https://maps.googleapis.com/maps/api/distancematrix/json?origins=Cannes&destinations=Nice&mode=driving&departure_time=now&traffic_model=best_guess&language=fr-FR&key=AIzaSyBfwmxGEnUSlilAXOkAkXbZeskaYJ5Gsak", function(error, response, body) {
-  //pessimistic, optimistic, best_guess;
-  try {
-    var obj = JSON.parse(body); //Parsing JSON
+  if ((dst=="")||(dep=="")) {
+    console.log("No itinary known");
+  }else{
+    var treshold=600;
+    request("https://maps.googleapis.com/maps/api/distancematrix/json?origins=Cannes&destinations=Nice&mode=driving&departure_time=now&traffic_model=best_guess&language=fr-FR&key=AIzaSyBfwmxGEnUSlilAXOkAkXbZeskaYJ5Gsak", function(error, response, body) {
+    //pessimistic, optimistic, best_guess;
+    try {
+      var obj = JSON.parse(body); //Parsing JSON
 
-    //Retrieve duration
-    var elements = obj.rows[0].elements;
-    var usual_time = elements[0].duration.value;
-    var current_time = elements[0].duration_in_traffic.value;
-    //define traffic condition
-    // var color="";
-    if (current_time-usual_time>=treshold) {
-      console.log("Bad traffic condition");
+      //Retrieve duration
+      var elements = obj.rows[0].elements;
+      var usual_time = elements[0].duration.value;
+      var current_time = elements[0].duration_in_traffic.value;
+      //define traffic condition
+      // var color="";
+      if (current_time-usual_time>=treshold) {
+        console.log("Bad traffic condition");
 
-      client.publish('mirror/led' , "red" ,{qos: 1})
-      // color="red";
-    }else if(current_time-usual_time>=-treshold){
-      console.log("Usual traffic condition ");
-       client.publish('mirror/led', "yellow",{qos: 1})
-      // color="red";
-    } else {
-      console.log("Excellent traffic condition ");
-      client.publish('mirror/led', "green",{qos: 1})
-      // color="red";
-    };
-  } catch (e){
-    console.error("Parsing error:",e);
+        client.publish('mirror/led' , "red" ,{qos: 1})
+        // color="red";
+      }else if(current_time-usual_time>=-treshold){
+        console.log("Usual traffic condition ");
+         client.publish('mirror/led', "yellow",{qos: 1})
+        // color="red";
+      } else {
+        console.log("Excellent traffic condition ");
+        client.publish('mirror/led', "green",{qos: 1})
+        // color="red";
+      };
+    } catch (e){
+      console.error("Parsing error:",e);
+    }
+    }); 
   }
-  }); 
+
 }
